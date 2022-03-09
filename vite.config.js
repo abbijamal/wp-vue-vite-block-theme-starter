418d1e2c5e0940b589cc238e022a36ea29f444a3
Paweł Madeja <4950739+pawelmadeja@users.noreply.github.com>
2022-03-10 00:12:08 +0100

    Make vite and wordpress work together

---
+++ vite.config.js
@@ -0,0 +1,66 @@
+// View your website at your own local server
+// for example http://vite-block-theme.test
+
+// http://localhost:3000 is serving Vite on development
+// but accessing it directly will be empty
+// TIP: consider changing the port for each project, see below
+
+// IMPORTANT image urls in CSS works fine
+// BUT you need to create a symlink on dev server to map this folder during dev:
+// ln -s {path_to_project_source}/src/assets {path_to_public_html}/assets
+// on production everything will work just fine
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import liveReload from "vite-plugin-live-reload";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    liveReload([
+      // edit live reload paths according to your source code
+      // for example:
+      __dirname + "/(parts|templates)/**/*.html",
+    ]),
+  ],
+
+  // config
+  root: "src",
+  // TODO: check it
+  // base: process.env.APP_ENV === "development" ? "/" : "/dist/",
+  base: "/",
+
+  build: {
+    // output dir for production build
+    outDir: "../dist",
+    emptyOutDir: true,
+
+    // emit manifest so PHP can find the hashed files
+    manifest: true,
+
+    // our entry
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/main.js"),
+    },
+  },
+
+  server: {
+    // required to load scripts from custom host
+    cors: true,
+
+    // we need a strict port to match on PHP side
+    // change freely, but update on PHP to match the same port
+    strictPort: true,
+    port: 3000,
+  },
+
+  // required for in-browser template compilation
+  // https://v3.vuejs.org/guide/installation.html#with-a-bundler
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
+  },
+});
